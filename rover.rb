e4b66e2aa4cb84c24bc143df0c9119b09d6270b0
SEILA <seila.tupone@gmail.com>
2016-06-02 12:22:12 -0400

    Created rover with var initialized

---
+++ rover.rb
@@ -0,0 +1,12 @@
+class Rover
+
+# access to the variables initialized
+attr_accessor :x, :y, :direction
+
+  # initialize the variables
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+
+end

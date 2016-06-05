3920bb869b9676a7df1b3b991d259cbdcccb93cf
SEILA <seila.tupone@gmail.com>
2016-06-05 10:26:34 -0400

    Added project. First version that needs to change

--- rover.rb
+++ rover.rb
@@ -3,100 +3,90 @@ class Rover
 # access to the variables initialized
 attr_accessor :x, :y, :direction
 
-  # initialize the variables
-  def initialize(x, y, direction)
-    @x = x
-    @y = y
-    @direction = direction
+  def gets_instruction
+
+    puts "What is the size of the plateau? Give me two numbers"
+    @size = gets.chomp
+    size_array = @size.split(" ")
+    size_x = size_array[0]
+    size_y = size_array[1]
+    @size = size_x + " " + size_y
+
+    puts "Where you want start?Give me two numbers and one coordinate"
+    @position_first = gets.chomp
+    position_first_array = @position_first.split(" ")
+    @x = position_first_array[0].to_i
+    @y = position_first_array[1].to_i
+    @direction = position_first_array[2]
+
+    read_instruction
+
   end
 
   def read_instruction
 
-    #attr_accessor :height, :weight
-
-    # puts "What is the size of the plateau? Give me two numbers"
-    #   @size = gets.chomp
-    #   size_array = @size.split(" ")
-    #   size_x = size_array[0]
-    #   size_y = size_array[1]
-    #   return size_x + " " + size_y
-
-    puts "Where you want start?Give me two numbers and one coordinate"
-      @position_first = gets.chomp
-      position_first_array = @position_first.split(" ")
-      @x = position_first_array[0]
-      @y = position_first_array[1]
-      @direction = position_first_array[2]
-
-      return @x + " " + @y + " " + @direction
-    #
-    # puts "Give me the combination of Move and Turn"
-    #   @combination_first = gets.chomp
-    #   combination_first_array = @combination_first.split(" ")
-    #   combination_first_array.map do |i|
-    #     if (i == 'M')
-    #
-    #     elsif (i == "M")
-    #       turn(choice_turn)
-    #     else
-    #       turn(choice_turn)
-    #   end
-
-    #
-    # puts "Where you want start?"
-    #   @position_second = gets.chomp
-    #
-    # puts "Give me the combination of Move and Turn"
-    #   @combination_second = gets.chomp
+    puts "Give me the combination of Move and Turn"
+    @combination_first = gets.chomp
+    combination_first_array = @combination_first.split(" ")
+    combination_first_array.each do |i|
+      if (i == 'M' || i == 'm')
+        move
+      elsif (i == "L" || i == 'l')
+        turn(i)
+      else
+        turn(i)
+      end
+    end
 
   end
-  #
-  # def turn(choice_turn)
-  #
-  #   if (choice_turn == 'L' && @direction == 'N')
-  #     @direction = 'W'
-  #   elsif (choice_turn == 'L' && @direction == 'E')
-  #     @direction = 'N'
-  #   elsif (choice_turn == 'L' && @direction == 'S')
-  #     @direction = 'W'
-  #   else
-  #     @direction = 'N'
-  #   end
-  #
-  #   if (choice_turn == 'R' && @direction == 'N')
-  #     @direction = 'E'
-  #   elsif (choice_turn == 'R' && @direction == 'E')
-  #     @direction = 'S'
-  #   elsif (choice_turn == 'R' && @direction == 'S')
-  #     @direction = 'E'
-  #   else
-  #     @direction = 'S'
-  #   end
-  #
-  #   # print the actual value of direction
-  #   return @direction
-  #
-  # end
-  #
-  # def move(change_position)
-  #
-  #   # case @direction
-  #   # when 'N'
-  #   #     @y += 1
-  #   # when 'S'
-  #   #     @y -= 1
-  #   # when 'E'
-  #   #     @x += 1
-  #   # else
-  #   #     @x -= 1
-  #   # end
-  #   #
-  #   # puts "the position is #{@x},#{@y}"
-  # end
 
+  def turn(choice_turn)
+
+    if (choice_turn == 'L' && @direction == 'N')
+      @direction = 'W'
+    elsif (choice_turn == 'L' && @direction == 'E')
+      @direction = 'N'
+    elsif (choice_turn == 'L' && @direction == 'S')
+      @direction = 'W'
+    else
+      @direction = 'N'
+    end
+
+    if (choice_turn == 'R' && @direction == 'N')
+      @direction = 'E'
+    elsif (choice_turn == 'R' && @direction == 'E')
+      @direction = 'S'
+    elsif (choice_turn == 'R' && @direction == 'S')
+      @direction = 'E'
+    else
+      @direction = 'S'
+    end
+
+  end
+
+  def move
+
+    case @direction
+    when 'N'
+        @y += 1
+    when 'S'
+        @y -= 1
+    when 'E'
+        @x += 1
+    else
+        @x -= 1
+    end
+
+  end
 
 
 end
 
-# Create an instance of Rover
-#rover = Rover.new(3, 4, 'N')
+
+rover1 = Rover.new
+rover1.gets_instruction
+puts "New rover position: #{rover1.x} #{rover1.y} #{rover1.direction}"
+
+rover2 = Rover.new
+rover2.gets_instruction
+puts "New rover position: #{rover2.x} #{rover2.y} #{rover2.direction}"
